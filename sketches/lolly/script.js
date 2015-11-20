87368998080ef67b8aa71c45e0c354ea9596110d
David Whitely <davydka@gmail.com>
2015-11-20 15:46:23 -0500

    Exploring 3d setup.

--- sketches/lolly/script.js
+++ sketches/lolly/script.js
@@ -39,20 +39,51 @@ function init() {
 
 	onWindowResize();
 
+	// model
+
+	var onProgress = function ( xhr ) {
+		if ( xhr.lengthComputable ) {
+			var percentComplete = xhr.loaded / xhr.total * 100;
+			console.log( Math.round(percentComplete, 2) + '% downloaded' );
+		}
+	};
+
+	var onError = function ( xhr ) {
+	};
+
+
+//	THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
+
+//	var loader = new THREE.OBJMTLLoader();
+//	loader.load( 'obj/male02.obj', 'obj/male02_dds.mtl', function ( object ) {
+
+//		object.position.y = - 80;
+//		scene.add( object );
+
+//	}, onProgress, onError );
+
 	group = new THREE.Object3D();
 
+	//scene.add(group);
+/*
 	var geometry = new THREE.CubeGeometry(10,20,3,3,6,2);
 	var modifier = new THREE.SubdivisionModifier( 3 );
-	modifier.modify( geometry );
+	//modifier.modify( geometry );
 	var mesh = new THREE.Mesh( geometry, material );
 	group.add( mesh );
 	var geometry = new THREE.CubeGeometry(2,13,0.3,2,10,8);
-	modifier.modify( geometry );
+	//modifier.modify( geometry );
 	var mesh = new THREE.Mesh( geometry, material );
-	mesh.position.y = -10;
+	mesh.position.y = -20;
+	group.add( mesh );
+*/
+	//var geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+	var geometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
+	var mesh = new THREE.Mesh( geometry, material );
 	group.add( mesh );
 
 	scene.add(group);
+
 
 	render();
 	
@@ -72,8 +103,10 @@ function render() {
 	
 	camera.lookAt( scene.position );
 
-	group.rotation.y += 0.06;
-	group.rotation.x += 0.03;
-	
+	if(typeof group != "undefined"){
+		group.rotation.y += 0.06;
+		group.rotation.x += 0.03;
+	}
+
 	renderer.render( scene, camera );
 }
